7491d2ed7a12151c987c22ced5303e397fcd4435
sindorim5 <kihunbuzz@naver.com>
2022-07-22 13:42:01 +0900

    0722 HW04 수정

--- Example_and_HW/src/com/ssafy/hw04/step3/ProductTest.java
+++ Example_and_HW/src/com/ssafy/hw04/step3/ProductTest.java
@@ -4,37 +4,38 @@ import java.util.ArrayList;
 
 public class ProductTest {
 
-    public static void main(String[] args) throws ProductNotFoundException, QuantityException {
-        IProductMgr mgr = ProductMgrImpl.getInstance();
-        ProductTest test = new ProductTest();
-        ArrayList<Product> list = mgr.getProductList();
+	public static void main(String[] args) throws ProductNotFoundException, QuantityException {
+		IProductMgr mgr = ProductMgrImpl.getInstance();
 
-        Product p1 = new Product(689, "Galaxy S21", 50, 100);
-        Product p2 = new Product(456, "Galaxy S22", 99, 150);
-        Product p3 = new TV(556, "OLED TV", 140, 50, 80, "OLED");
-        Product p4 = new TV(779, "LCD TV", 100, 70, 35, "LCD");
-        Product p5 = new TV(779, "SAMSUNG TV", 100, 30, 65, "LED");
-        Product p6 = new Refrigerator(321, "DIOS", 119, 40, 324);
-        Product p7 = new Refrigerator(232, "BESPOKE", 180, 30, 522);
-        Product p8 = new Refrigerator(235, "SAMSUNG", 150, 25, 600);
+		ProductTest test = new ProductTest();
+		ArrayList<Product> list = mgr.getProductList();
 
-        // add
-        System.out.println();
+		Product p1 = new Product(689, "Galaxy S21", 50, 100);
+		Product p2 = new Product(456, "Galaxy S22", 99, 150);
+		Product p3 = new TV(556, "OLED TV", 140, 50, 80, "OLED");
+		Product p4 = new TV(779, "LCD TV", 100, 70, 35, "LCD");
+		Product p5 = new TV(779, "SAMSUNG TV", 100, 30, 65, "LED");
+		Product p6 = new Refrigerator(321, "DIOS", 119, 40, 324);
+		Product p7 = new Refrigerator(232, "BESPOKE", 180, 30, 522);
+		Product p8 = new Refrigerator(235, "SAMSUNG", 150, 25, 600);
 
-        mgr.addProduct(p1);
-        mgr.addProduct(p2);
-        mgr.addProduct(p3);
-        mgr.addProduct(p4);
-        mgr.addProduct(p5);
-        mgr.addProduct(p6);
-        mgr.addProduct(p7);
-        mgr.addProduct(p8);
+		// add
+		System.out.println();
 
-        System.out.println("*************************전체상품*************************");
-        test.printProductList(list);
-        System.out.println();
+		mgr.addProduct(p1);
+		mgr.addProduct(p2);
+		mgr.addProduct(p3);
+		mgr.addProduct(p4);
+		mgr.addProduct(p5);
+		mgr.addProduct(p6);
+		mgr.addProduct(p7);
+		mgr.addProduct(p8);
 
-        mgr.saveData();
+		System.out.println("*************************전체상품*************************");
+		test.printProductList(list);
+		System.out.println();
+
+		mgr.saveData();
 //
 //		// 상품번호로 상품 찾기
 //		System.out.println("*************************상품번호: 456*************************");
@@ -117,13 +118,13 @@ public class ProductTest {
 //		test.printProductList(mgr.getProductList());
 //		System.out.println();
 
-    }
+	}
 
-    public <T> void printProductList(ArrayList<T> pList) {
-        for (T p : pList) {
-            if (p != null) {
-                System.out.println(p);
-            }
-        }
-    }
+	public <T> void printProductList(ArrayList<T> pList) {
+		for (T p : pList) {
+			if (p != null) {
+				System.out.println(p);
+			}
+		}
+	}
 }
