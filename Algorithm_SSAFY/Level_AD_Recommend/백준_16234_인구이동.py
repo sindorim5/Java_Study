2de5a7dd6a2167769e1a4cc55570b89f9e6c7cff
Kihun SONG <kihunbuzz@naver.com>
2022-08-27 22:41:49 +0900

    0827

--- Algorithm_SSAFY/Level_AD_Recommend/백준_16234_인구이동.py
+++ Algorithm_SSAFY/Level_AD_Recommend/백준_16234_인구이동.py
@@ -4,49 +4,49 @@ from collections import deque
 sys.stdin = open("input.txt", "r", encoding="UTF-8")
 
 N, L, R = map(int, input().split())
-
 matrix = [list(map(int, input().split())) for _ in range(N)]
 
 dy = [0, 0, 1, -1]
 dx = [1, -1, 0, 0]
 
-count = 0
-changeList = []
-
 def bfs(y, x):
     q = deque()
-    changeList = [(y,x)]
-    sumValue = 0
+    changeList = []
     q.append((y, x))
-    visited = [[False] * N for _ in range(N)]
+    changeList.append((y, x))
     visited[y][x] = True
     while q:
-        nowY, nowX = q.popleft()
-        sumValue += matrix[nowY][nowX]
+        b, a = q.popleft()
         for i in range(4):
-            nY = nowY + dy[i]
-            nX = nowX + dx[i]
-            if 0 <= nY < N and 0 <= nX < N:
-                if not visited[nY][nX] and L <= abs(matrix[nY][nX] - matrix[nowY][nowX]) <= R:
+            nY = b + dy[i]
+            nX = a + dx[i]
+            if 0 <= nY < N and 0 <= nX < N and not visited[nY][nX]:
+                if L <= abs(matrix[nY][nX] - matrix[b][a]) <= R:
                     visited[nY][nX] = True
                     changeList.append((nY, nX))
                     q.append((nY, nX))
-    print(changeList)
-    changeValue = sumValue // len(changeList)
-    print(changeValue)
-    for change in changeList:
-        i, j = change
-        matrix[i][j] = changeValue
+    return changeList
 
-
+count = 0
 while True:
-    temp = 0
-    for y in range(N - 1):
-        for x in range(N - 1):
-            if L <= abs(matrix[y][x] - matrix[y][x + 1]) <= R or L <= abs(matrix[y][x] - matrix[y + 1][x]) <= R:
-                bfs(y, x)
-                temp += 1
+    flag = 0
+    temp = []
+    visited = [[False] * N for _ in range(N)]
+    for y in range(N):
+        for x in range(N):
+            if not visited[y][x]:
+                visited[y][x] = True
+                temp = bfs(y, x)
+                if len(temp) > 1:
+                    flag = 1
+                    changeValue = 0
+                    for t in temp:
+                        changeValue += matrix[t[0]][t[1]]
+                    changeValue = changeValue // len(temp)
+                    for t in temp:
+                        matrix[t[0]][t[1]] = changeValue
+    if flag == 0:
+        break
+    count += 1
 
-    if L <= abs(matrix[N-1][N-1] - matrix[N-1][N-2]) <= R or L <= abs(matrix[N-1][N-1] - matrix[N-2][N-1]) <= R:
-        bfs(y, x)
-        temp += 1
+print(count)
